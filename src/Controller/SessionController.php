e348d71fdc0926e80ce32c1adfe90dbcf2bd542a
cem-sucu <cems.cems.17@gmail.com>
2023-09-12 15:18:38 +0200

    affichage de la liste des Sessions

--- src/Controller/SessionController.php
+++ src/Controller/SessionController.php
@@ -2,17 +2,22 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\Session;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class SessionController extends AbstractController
 {
     #[Route('/session', name: 'app_session')]
-    public function index(): Response
+    // EntityManagerInterface on l'importe
+    public function index(EntityManagerInterface $entityManager): Response
     {
+        // j'oublie pas de importer Session : App\Entity
+        $sessions =$entityManager->getRepository(Session::class)->findAll();
         return $this->render('session/index.html.twig', [
-            'controller_name' => 'SessionController',
+            'sessions' => $sessions,
         ]);
     }
 }
