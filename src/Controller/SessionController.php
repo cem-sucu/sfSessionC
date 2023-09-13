33f8485d0ad09790a390eef682da390791c8582b
cem-sucu <cems.cems.17@gmail.com>
2023-09-13 11:44:06 +0200

    affichage de la page module et de stagiaire

--- src/Controller/SessionController.php
+++ src/Controller/SessionController.php
@@ -23,3 +23,4 @@ class SessionController extends AbstractController
         ]);
     }
 }
+
--- src/Controller/StagiaireController.php
+++ src/Controller/StagiaireController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\StagiaireRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,10 +10,11 @@ use Symfony\Component\Routing\Annotation\Route;
 class StagiaireController extends AbstractController
 {
     #[Route('/stagiaire', name: 'app_stagiaire')]
-    public function index(): Response
+    public function index(StagiaireRepository $stagiaireRepository): Response
     {
+        $stagiaires = $stagiaireRepository->findAll();
         return $this->render('stagiaire/index.html.twig', [
-            'controller_name' => 'StagiaireController',
+            'stagiaires' => $stagiaires,
         ]);
     }
 }
