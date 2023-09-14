eea40a79d790869b6e28046052595f4f8a1997a2
cem-sucu <cems.cems.17@gmail.com>
2023-09-14 16:00:55 +0200

    ajout du formulaire pour ajouter un stagiaire fonctionnel + message flash

--- src/Controller/SessionController.php
+++ src/Controller/SessionController.php
@@ -22,10 +22,12 @@ class SessionController extends AbstractController
     }
 
     #[Route('/session/{id}', name: 'show_session')]
-    public function show(Session $session): Response
+    public function show(Session $session, SessionRepository $sr): Response
     {
+        $nonInscrits = $sr->findNonInscrits($session->getId());
         return $this->render('session/show.html.twig', [
             'session'=>$session,
+            'nonInscrits' => $nonInscrits
         ]);
     }
 }
--- src/Controller/StagiaireController.php
+++ src/Controller/StagiaireController.php
@@ -3,7 +3,10 @@
 namespace App\Controller;
 
 use App\Entity\Stagiaire;
+use App\Form\StagiaireType;
 use App\Repository\StagiaireRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -19,6 +22,34 @@ class StagiaireController extends AbstractController
         ]);
     }
 
+
+   // pour le fomurlaire
+    #[Route('/stagiaire/new', name: 'new_stagiaire')]
+    //EntityManagerInterface $entityManager on le met en place pour le formulaire et on l'importe
+    public function new (Request $request, EntityManagerInterface $entityManager): Response
+    {
+        $stagiaire = new Stagiaire();
+        $form = $this->createForm(StagiaireType::class, $stagiaire);
+        $form->handleRequest($request);
+
+        //si formulaire soumis est valide
+        if ($form->isSubmitted() && $form->isValid()) {
+            $stagiaire = $form->getData();
+            //l'équivalent de prepare PDO
+            $entityManager->persist($stagiaire);
+            // l'équivalent du execute en PDO
+            $entityManager->flush();
+            // Ajouter un message flash
+            $this->addFlash('success', 'Stagiaire bien ajouté');
+            return $this->redirectToRoute('app_stagiaire');
+        }
+
+        return $this->render('stagiaire/new.html.twig', [
+            'formAddStagiaire'=>$form,
+        ]);
+    }
+
+    //placer la route ID toujours a la fin
     #[Route('/stagiaire/{id}', name: 'show_stagiaire')]
     public function show(Stagiaire $stagiaire): Response
     {
@@ -26,4 +57,7 @@ class StagiaireController extends AbstractController
             'stagiaire' => $stagiaire,
         ]);
     }
+
+ 
+
 }
