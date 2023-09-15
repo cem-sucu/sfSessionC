22b7649782f8026a758a14ae3456164d5792ae09
cem-sucu <cems.cems.17@gmail.com>
2023-09-15 15:52:46 +0200

    ajouter les stagiaire non inscrit dans une session

--- src/Controller/SessionController.php
+++ src/Controller/SessionController.php
@@ -3,8 +3,10 @@
 namespace App\Controller;
 
 use App\Entity\Session;
+use App\Entity\Stagiaire;
 use App\Repository\SessionRepository;
 use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -24,15 +26,34 @@ class SessionController extends AbstractController
     // pour afficher la liste des nonInscrits
     // et pour afficher les module dispo mais pas présent dans la session
     #[Route('/session/{id}', name: 'show_session')]
-    public function show(Session $session, SessionRepository $sr): Response
+    public function show(Session $session, Request $request, EntityManagerInterface $entityManager, SessionRepository $sessionRepository): Response
     {
-        $nonInscrits = $sr->findNonInscrits($session->getId());
-        $moduleDispo = $sr->findModuleDispo($session->getId());
-        return $this->render('session/show.html.twig', [
-            'session'=>$session,
-            'nonInscrits' => $nonInscrits,
-            'moduleDispo' => $moduleDispo,
-        ]);
+        // je vérifie si la méthode est de type post
+        if ($request->isMethod('POST')) {
+            //je recupere le ID du stagiaire
+            $stagiaireId = $request->request->get('stagiaire_id');
+            // on vérifie que le stagiaire n'est pas déjà inscrit dans la session
+            $stagiaire = $entityManager->getRepository(Stagiaire::class)->find($stagiaireId);
+            //si stagiaire existe et pas encore inscrit donc visible dans la liste des non inscrit
+            if ($stagiaire && !$session->getStagiaires()->contains($stagiaire)) {
+                //alors on ajoute le stagiaire a la session
+                $session->addStagiaire($stagiaire);
+                $entityManager->flush();
+                $this->addFlash('success-message', 'Stagiaire ajouté');
+            } else {
+                $this->addFlash('error-message', 'Ce stagiaire est déjà inscrit à la session ou n\'existe pas.');
+            }
+        }
+    
+    $nonInscrits = $sessionRepository->findNonInscrits($session->getId());
+    $moduleDispo = $sessionRepository->findModuleDispo($session->getId());
+    
+    return $this->render('session/show.html.twig', [
+        'session' => $session,
+        'nonInscrits' => $nonInscrits,
+        'moduleDispo' => $moduleDispo,
+    ]);
     }
-}
 
+
+}
