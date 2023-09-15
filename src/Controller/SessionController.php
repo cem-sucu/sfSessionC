11c8c60685a1e9eeafee7928ac8a54083735b518
cem-sucu <cems.cems.17@gmail.com>
2023-09-15 16:38:56 +0200

    function inscrire un stagiaire et supprimer un stagiaire opérationel

--- src/Controller/SessionController.php
+++ src/Controller/SessionController.php
@@ -23,37 +23,78 @@ class SessionController extends AbstractController
         ]);
     }
 
+    // #[Route('/session/{id}', name: 'show_session')]
+    // public function desinscrireStagiaire(Session $session, Stagiaire $stagiaire, EntityManagerInterface $entityManager): Response
+    // {
+    //     // comme pour l'ajout je vérifie si le stagiaire est inscrit dans la session
+    //     if ($session->getStagiaires()->contains($stagiaire)) {
+    //         //sil est ionscrti dans la session je le supprimer/désinscrit
+    //         $session->removeStagiaire($stagiaire);
+    //         $entityManager->flush();
+    //         $this->addFlash('success-message', 'Vous avez bien déinscrit le/la stagiaire.');
+    //     } else {
+            
+    //     }
+
+    //     return $this->redirectToRoute('show_session', ['id' => $session->getId()]);
+    // }
+
+
     // pour afficher la liste des nonInscrits
     // et pour afficher les module dispo mais pas présent dans la session
     #[Route('/session/{id}', name: 'show_session')]
     public function show(Session $session, Request $request, EntityManagerInterface $entityManager, SessionRepository $sessionRepository): Response
     {
-        // je vérifie si la méthode est de type post
+        // Vérifie si la requête est de type POST lorsque le formulaire est soumis
         if ($request->isMethod('POST')) {
-            //je recupere le ID du stagiaire
+            // Action d'ajout ou de désinscription
+            $action = $request->request->get('action'); 
             $stagiaireId = $request->request->get('stagiaire_id');
-            // on vérifie que le stagiaire n'est pas déjà inscrit dans la session
-            $stagiaire = $entityManager->getRepository(Stagiaire::class)->find($stagiaireId);
-            //si stagiaire existe et pas encore inscrit donc visible dans la liste des non inscrit
-            if ($stagiaire && !$session->getStagiaires()->contains($stagiaire)) {
-                //alors on ajoute le stagiaire a la session
-                $session->addStagiaire($stagiaire);
-                $entityManager->flush();
-                $this->addFlash('success-message', 'Stagiaire ajouté');
-            } else {
-                $this->addFlash('error-message', 'Ce stagiaire est déjà inscrit à la session ou n\'existe pas.');
+    
+            // si l'action est désinscrire 
+            if ($action === 'inscrire') {
+                // on recherche un stagiaire selon son id
+                $stagiaire = $entityManager->getRepository(Stagiaire::class)->find($stagiaireId);
+    
+                // si stagiaire existe et n'est pas insccrirt 
+                if ($stagiaire && !$session->getStagiaires()->contains($stagiaire)) {
+                    //on ajoute le stagiaire a la session
+                    $session->addStagiaire($stagiaire);
+                    // on enregistre dans la bdd 
+                    // on tire la chasse d'eau
+                    $entityManager->flush();
+                    $this->addFlash('success-message', 'Stagiaire ajouté à la session.');
+                } else {
+                    $this->addFlash('error-message', 'Ce stagiaire est déjà inscrit à la session ou n\'existe pas.');
+                }
+                //sinon si action = descincrire
+            } elseif ($action === 'desinscrire') {
+                // on recherche un stagiaire selon son id
+                $stagiaire = $entityManager->getRepository(Stagiaire::class)->find($stagiaireId);
+    
+                // si stagiaire est inscrit dans la session
+                if ($session->getStagiaires()->contains($stagiaire)) {
+                    // on retire le stagiaire inscrit
+                    $session->removeStagiaire($stagiaire);
+                    // et on enregistre aussi les donné en bdd
+                    // on tire la chasse d'eau
+                    $entityManager->flush();
+                    $this->addFlash('success-message', 'Stagiaire désinscrit de la session.');
+                } else {
+                    $this->addFlash('error-message', 'Ce stagiaire n\'est pas inscrit à la session.');
+                }
             }
         }
     
-    $nonInscrits = $sessionRepository->findNonInscrits($session->getId());
-    $moduleDispo = $sessionRepository->findModuleDispo($session->getId());
+        $nonInscrits = $sessionRepository->findNonInscrits($session->getId());
+        $moduleDispo = $sessionRepository->findModuleDispo($session->getId());
     
-    return $this->render('session/show.html.twig', [
-        'session' => $session,
-        'nonInscrits' => $nonInscrits,
-        'moduleDispo' => $moduleDispo,
-    ]);
+        return $this->render('session/show.html.twig', [
+            'session' => $session,
+            'nonInscrits' => $nonInscrits,
+            'moduleDispo' => $moduleDispo,
+        ]);
     }
-
+    
 
 }
